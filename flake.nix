d62538b001b3c7deb89de9ea31808b2a56695f0b
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2026-04-16 13:19:25 +1200

    Update dev shell to eliminate flake-utils and fix dependencies
    This brings in changes tested in arconyx/hexchanting
    b5b41030d10bbb47135898131d3e6806f048fa71 to modernise the dev shell.

--- flake.nix
+++ flake.nix
@@ -1,20 +1,38 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
   outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ stdenv.cc.cc.lib jdk21 ];
-          LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath [ pkgs.openal pkgs.glfw pkgs.libGL pkgs.flite ]; 
-        };
-      }
-    );
+    { nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+      # f is a function `system (str) -> packages (attrset) -> attrset`
+      forAllSystems =
+        f: lib.genAttrs lib.systems.flakeExposed (system: f system nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (
+        system: pkgs: {
+          default =
+            let
+              libs = with pkgs; [
+                libGL
+                glfw3-minecraft
+                pipewire
+                flite
+              ];
+              java = pkgs.jdk21;
+            in
+            pkgs.mkShell {
+              nativeBuildInputs = [ java ];
+
+              buildInputs = libs;
+
+              env = {
+                LD_LIBRARY_PATH = lib.makeLibraryPath libs;
+                JAVA_HOME = java.home;
+              };
+            };
+        }
+      );
+    };
 }
